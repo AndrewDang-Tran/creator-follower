3431b18b40fd6c4d11451ba7b2a419ad0b39c400
Andrew Dang-Tran <andrewdt1506@gmail.com>
2022-07-02 07:54:50 -0700

    feat(): support errors over panics

--- cf-server/src/errors.rs
+++ cf-server/src/errors.rs
@@ -3,7 +3,15 @@ use actix_web::{
     http::{StatusCode, header::ContentType},
     HttpResponse,
 };
+use std::error::Error as StdError;
 use derive_more::{Display, Error};
+
+#[derive(Debug, Display)]
+pub struct ErrorMessageWrapper {
+    message: &'static str
+}
+
+impl StdError for ErrorMessageWrapper {}
 
 
 #[derive(Debug, Display, Error)]
@@ -11,8 +19,11 @@ pub enum ServiceError {
     #[display(fmt = "An internal error occurred. Please try again Later")]
     InternalError,
 
-    #[display(fmt = "Unexpected Anilist data format")]
-    AnilistDataFormat,
+    #[display(fmt = "Unexpected Anilist data format: {}", _0)]
+    AnilistDataFormat(ErrorMessageWrapper),
+
+    #[display(fmt = "An internal error occurred: {}", _0)]
+    InternalLogicError(ErrorMessageWrapper)
 }
 
 impl error::ResponseError for ServiceError {
@@ -23,9 +34,18 @@ impl error::ResponseError for ServiceError {
     }
 
     fn status_code(&self) -> StatusCode {
-        match *self {
+        match &self {
             ServiceError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
-            ServiceError::AnilistDataFormat => StatusCode::INTERNAL_SERVER_ERROR,
+            ServiceError::AnilistDataFormat(_e) => StatusCode::INTERNAL_SERVER_ERROR,
+            ServiceError::InternalLogicError(_e) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
+}
+
+pub fn anilist_data_format(message: &'static str) ->  ServiceError {
+    ServiceError::AnilistDataFormat(ErrorMessageWrapper { message })
+}
+
+pub fn internal_logic_error(message: &'static str) -> ServiceError {
+    ServiceError::InternalLogicError(ErrorMessageWrapper { message })
 }
--- cf-server/src/routes/anilist_routes.rs
+++ cf-server/src/routes/anilist_routes.rs
@@ -4,11 +4,13 @@ use graphql_client::{GraphQLQuery, Response};
 use rss::{Channel, ChannelBuilder, Item, ItemBuilder, ImageBuilder, Image};
 use reqwest;
 use chrono::naive::NaiveDate;
-use super::super::errors::ServiceError;
+use self::staff_media_query::StaffMediaQueryStaffStaffMediaNodes;
+use super::super::{errors, errors::ServiceError};
 
 const ANILIST_GRAPHQL_URL: &str = "https://graphql.anilist.co";
 const RSS_2_SPECIFICATION_URL: &str = "https://validator.w3.org/feed/docs/rss2.html";
 const NO_STAFF_DESCRIPTION: &str = "No description provided by Anilist for this staff.";
+const STAFF_NONE: &str = "Staff is None";
 
 #[derive(GraphQLQuery)]
 #[graphql(
@@ -49,36 +51,41 @@ async fn get_anilist_rss_feed(path: web::Path<i64>,
         "status": "FINISHED"
       },
       */
-    let staff = response_body.data.ok_or(ServiceError::InternalError)?
-        .staff.ok_or(ServiceError::InternalError)?;
+    let staff = response_body.data.ok_or(errors::anilist_data_format("Data is None"))?
+        .staff.ok_or(errors::anilist_data_format(STAFF_NONE))?;
     let media = staff
-        .staff_media.ok_or(ServiceError::InternalError)?
-        .nodes.ok_or(ServiceError::InternalError)?;
-    let anilist_staff_name = staff.name.ok_or(ServiceError::InternalError)?;
-
+        .staff_media.ok_or(errors::anilist_data_format("Staff.staffMedia is None"))?
+        .nodes.ok_or(errors::anilist_data_format("Staff.staffMedia.nodes is None"))?;
+    let anilist_staff_name = staff.name.ok_or(errors::anilist_data_format("Staff.name is None"))?;
     let staff_name_collection: Vec<Option<String>> = vec![anilist_staff_name.full, anilist_staff_name.native];
     let staff_name: String = staff_name_collection.into_iter()
         .filter(|name| name.is_some())
-        .map(|name| name.expect(""))
-        .collect::<Vec<String>>()
+        .map(|name| name.ok_or(errors::internal_logic_error("name cannot be None after is_some() check")))
+        .collect::<Result<Vec<String>, ServiceError>>()?
         .join(", ");
 
     let staff_channel_items: Vec<Item> = media.into_iter()
-        .filter_map(|m| {
+        .map(|m| {
             let has_media = m.is_some();
+            if !has_media {
+              return Ok(None)
+            }
             let start_date = m.as_ref()
-                .expect("Anilist does not allow None start_date")
+                .ok_or(errors::internal_logic_error("Staff.staffMedia[] is None"))?
                 .start_date
-                .as_ref().expect("Anilist should always have it"); //TODO: replace with anilist error
+                .as_ref().ok_or(errors::anilist_data_format("Staff.staffMedia.startDate"))?;
             let has_start_date = start_date.year.is_some() || start_date.month.is_some() || start_date.day.is_some();
-            if has_media && has_start_date {
-                Some(m)
+            if has_start_date {
+              Ok(m)
             } else {
-                None
+              Ok(None)
             }
         })
-        .map(|x| {
-            let m = x.expect("Logically cannot be None");
+        .collect::<Result<Vec<Option<StaffMediaQueryStaffStaffMediaNodes>>, ServiceError>>()?
+        .into_iter()
+        .filter(|o_m| o_m.is_some())
+        .map(|o| {
+            let m = o.ok_or(errors::internal_logic_error("Logically cannot be None"))?;
             let mut title: String = "Anilist has no title".to_string();
             if let Some(t) = m.title {
                 let mut english = t.romaji;
@@ -94,23 +101,23 @@ async fn get_anilist_rss_feed(path: web::Path<i64>,
                     .join(", ");
             }
 
-            let start_date = m.start_date.as_ref().expect("Anilist does not allow None start_date");
-            let year = start_date.year.expect("Year cannot be None");
-            let month = start_date.month.expect("Month cannot be None");
-            let day = start_date.day.expect("Day cannot be None");
+            let start_date = m.start_date.as_ref().ok_or(errors::internal_logic_error("Staff.staffMedia[].startDate is None after check"))?;
+            let year = start_date.year.ok_or(errors::internal_logic_error("Staff.staffMedia[].startDate.year is None after check"))?;
+            let month = start_date.month.ok_or(errors::internal_logic_error("Staff.staffMedia[].startDate.month is None after check"))?;
+            let day = start_date.day.ok_or(errors::internal_logic_error("Staff.staffMedia[].startDate.day is None after check"))?;
             let naive_date = NaiveDate::from_ymd(year as i32, month as u32, day as u32);
 
-            ItemBuilder::default()
-                .title(title)
+            Ok(ItemBuilder::default()
+                .title(Some(title))
                 .link(m.site_url)
                 .description(m.description)
-                .pub_date(naive_date.to_string())
-                .build()
-        }).collect();
+                .pub_date(Some(naive_date.to_string()))
+                .build())
+        }).collect::<Result<Vec<Item>, ServiceError>>()?;
 
-    let site_url = staff.site_url.ok_or(ServiceError::AnilistDataFormat)?;
-    let image_url = staff.image.ok_or(ServiceError::AnilistDataFormat)?
-      .large.ok_or(ServiceError::AnilistDataFormat)?;
+    let site_url = staff.site_url.ok_or(errors::anilist_data_format("Staff.siteUrl is None"))?;
+    let image_url = staff.image.ok_or(errors::anilist_data_format("Staff.image is None"))?
+      .large.ok_or(errors::anilist_data_format("Staff.image.large is None"))?;
     let rss_image: Image = ImageBuilder::default()
       .title(&staff_name)
       .link(&site_url)
@@ -118,7 +125,7 @@ async fn get_anilist_rss_feed(path: web::Path<i64>,
       .build();
     let mut staff_description = NO_STAFF_DESCRIPTION.to_string();
     if staff.description.is_some() {
-      staff_description = staff.description.expect("staff.description cannot be None");
+      staff_description = staff.description.ok_or(errors::internal_logic_error("staff.description cannot be None"))?;
     }
 
     let staff_channel: Channel = ChannelBuilder::default()
