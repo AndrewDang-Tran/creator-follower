71a020ff69497da09e6c0dd737cac3b3d95fbf8d
Andrew Dang-Tran <andrewdt1506@gmail.com>
2022-07-29 18:56:39 -0700

    feat(): style front end main page and add some short details about the project

--- cf-server/src/main.rs
+++ cf-server/src/main.rs
@@ -44,6 +44,8 @@ async fn main() -> std::io::Result<()> {
             .configure(routes::init_health_routes)
             .configure(routes::init_anilist_routes)
             .service(index)
+            .service(actix_files::Files::new("static/js", "static/js").show_files_listing())
+            .service(actix_files::Files::new("static/css", "static/css").show_files_listing())
     })
     .bind(("0.0.0.0", 8080))?
     .run()
